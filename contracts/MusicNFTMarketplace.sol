f59cf768cee87c5a780c0684bc80313bcf570ad1
HZZX0318 <zexuanzhou2025@u.northwestern.edu>
2022-12-29 11:59:36 -0600

    12.29 update

---
+++ contracts/MusicNFTMarketplace.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract MusicNFTMarketplace is ERC721("harry", "zhou"), Ownable {
+    string public baseURI =
+        "https://bafybeidhjjbjonyqcahuzlpt7sznmh4xrlbspa3gstop5o47l6gsiaffee.ipfs.nftstorage.link/";
+    string public baseExtension = ".json";
+    address public artist;
+    uint256 public royaltyFee;
+}
